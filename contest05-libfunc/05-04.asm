d917757c0f7d3802e1c482d6b3e9b9ca6edf82bc
Elk Cloner <dev@elkcl.ru>
2023-04-05 11:46:35 +0300

    05-04 segfault

--- contest05-libfunc/05-04.asm
+++ contest05-libfunc/05-04.asm
@@ -1,9 +1,12 @@
-extern fopen, fscanf, printf
+extern fopen, fclose, fscanf, printf
+
+%define EOF -1
 
 section .rodata
-    ifmt: db "%d"
-    ofmt: db "%d\n"
-    filename: db "data.in"
+    ifmt: db "%*d", 0
+    ofmt: db "%d\n", 0
+    filename: db "data.in", 0
+    mode: db "r", 0
 
 section .bss
     handle: resd 1
@@ -12,7 +15,28 @@ section .text
 global main
 main:
     enter 8, 0
-    
+    mov dword [esp], filename
+    mov dword [esp+4], mode
+    call fopen
+    mov [handle], eax
+    xor ebx, ebx
+.L1:
+    mov eax, [handle]
+    mov [esp], eax
+    mov dword [esp+4], ifmt
+    call fscanf
+    inc ebx
+    cmp eax, EOF
+    jne .L1
+
+    dec ebx
+    mov dword [esp], ofmt
+    mov [esp+4], ebx
+    call printf
+
+    mov eax, [handle]
+    mov [esp], eax
+    call fclose
     xor eax, eax
     leave
     ret
