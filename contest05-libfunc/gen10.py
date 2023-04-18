ef9ef9f5d79406422fbe0a0ae1f0275b9d88abdb
Elk Cloner <dev@elkcl.ru>
2023-04-18 20:56:34 +0300

    05-10 mrf

--- contest05-libfunc/gen10.py
+++ contest05-libfunc/gen10.py
@@ -4,9 +4,9 @@ q = randint(0, 10)
 for _ in range(q):
     c = choice(('A', 'D', 'S'))
     if c == 'A':
-        print('A {} {}', randint(-10, 10), randint(-10, 10))
+        print("A {} {}".format(randint(-10, 10), randint(-10, 10)))
     elif c == 'D':
-        print('D {}', randint(-10, 10))
+        print("D {}".format(randint(-10, 10)))
     else:
-        print('S {}', randint(-10, 10))
+        print("S {}".format(randint(-10, 10)))
 print('F')
